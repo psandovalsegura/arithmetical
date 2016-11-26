6832f04fa3334035c644150521dbffa9a9216954
Pedro Sandoval <psandovalsegura@gmail.com>
2016-11-25 17:53:31 -0800

    Complete basic functionality of prime factorization

--- arithmetical/DragGamePlayViewController.swift
+++ arithmetical/DragGamePlayViewController.swift
@@ -13,19 +13,30 @@ class DragGamePlayViewController: UIViewController {
     var game: DragGame!
     var option: String!
     var currentNumber: String!
+    @IBOutlet weak var checkmarkImageView: UIImageView!
+    @IBOutlet weak var correctLabel: UILabel!
     
     //Mark -- Timer
     var timer = Timer()
     var timerSeconds = Games.timerSeconds // 120 seconds = 2 minutes
     let timerDecrement = 1
     
-    var correctResponses = 0
+    var correctResponses = 0 {
+        didSet {
+            self.correctLabel.text = String(correctResponses)
+        }
+    }
     var previousQuestions:[[String]] = []
     var studyQuestions: [[String]] = []
 
-    var mainNumber: Int?
+    var mainNumber: Int? {
+        didSet {
+            self.mainNumberLabel.text = String(mainNumber!)
+        }
+    }
     @IBOutlet weak var mainNumberLabel: UILabel!
     var newPrime: UILabel!
+    var mainNumberScale = 1.0
     
     
     override func viewDidLoad() {
@@ -33,22 +44,43 @@ class DragGamePlayViewController: UIViewController {
         self.title = game.name!
         
         // Do any additional setup after loading the view.
+        self.correctLabel.text = String(self.correctResponses)
         self.presentQuestion()
     }
     
+    
+    
     func presentQuestion() {
-        
+        //Ensure the number is composite
         self.mainNumber = self.game.mainNumberGenerator()
-        self.mainNumberLabel.text = String(self.mainNumber!)
-        
+        while isPrime(number: self.mainNumber!) {
+            self.mainNumber = self.game.mainNumberGenerator()
+        }
     }
 
-    func validateAnswer() {
+    func validateAnswer() -> Bool {
+        if self.mainNumber! % Int(self.newPrime.text!)! == 0 {
+            return true
+        }
+        return false
+    }
+    
+    func isPrime(number: Int) -> Bool {
+        //Check numbers up to the square root of the number
         
+        var check: Int = 2
+        while Double(check) <= round(pow(Double(number), 0.5)) {
+            if number % check == 0 {
+                return false
+            }
+            check += 1
+        }
+        
+        return true
     }
     
     @IBAction func didPanPrime(_ sender: UIPanGestureRecognizer) {
-        var createdPrime = sender.view as! UILabel
+        let createdPrime = sender.view as! UILabel
         let translation = sender.translation(in: self.view)
         
         switch sender.state {
@@ -61,10 +93,9 @@ class DragGamePlayViewController: UIViewController {
             self.newPrime.font = createdPrime.font
             self.newPrime.textColor = createdPrime.textColor
             
-            //Add the new label to the view
+            //Add the new label to the view, for dragging
             self.view.addSubview(self.newPrime)
             self.newPrime.center = createdPrime.center
-            
             UIView.animate(withDuration: 0.1, animations: {
                 self.newPrime.transform = CGAffineTransform(scaleX: 2, y: 2)
             }, completion: { (done) in
@@ -79,17 +110,63 @@ class DragGamePlayViewController: UIViewController {
             //Check if drop is valid
             if !self.validPrimePosition() {
                 self.newPrime.removeFromSuperview()
+            } else if !validateAnswer() {
+                self.mainNumberLabel.shake()
+                animatePrimeFalling()
             } else {
-                UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: {
-                    self.newPrime.transform = CGAffineTransform(scaleX: 1, y: 1)
-                }, completion: { (done) in
-                    //
-                })
+                //Update the main number
+                self.mainNumberScale -= 0.2
+                self.mainNumber = self.mainNumber! / Int(self.newPrime.text!)!
+                
+                //Check if the number has been completely factored
+                if self.mainNumber == 1 {
+                    animateCorrectCheckmark()
+                    correctResponses += 1
+                    resetView()
+                    self.presentQuestion()
+                } else {
+                    UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 1, options: [], animations: {
+                        self.mainNumberLabel.transform = CGAffineTransform(scaleX: CGFloat(self.mainNumberScale), y: CGFloat(self.mainNumberScale))
+                    }, completion: { (done) in
+                        //
+                    })
+                    
+                }
+                
+                animatePrimeFalling()
             }
             
         default:
             print("default")
         }
+    }
+    
+    func animateCorrectCheckmark() {
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.checkmarkImageView.isHidden = false
+            self.checkmarkImageView.alpha = 0.0
+        }, completion: { (finished) in
+            self.checkmarkImageView.isHidden = true
+            self.checkmarkImageView.alpha = 1.0
+        })
+    }
+    
+    func resetView() {
+        self.mainNumberScale = 1
+        UIView.animate(withDuration: 0.2, animations: {
+            self.mainNumberLabel.transform = CGAffineTransform(scaleX: CGFloat(self.mainNumberScale), y: CGFloat(self.mainNumberScale))
+        })
+    }
+    
+    //Animates a falling prime and also removes it from view
+    func animatePrimeFalling() {
+        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: {
+            self.newPrime.transform = CGAffineTransform(scaleX: 1, y: 1)
+            self.newPrime.alpha = 0.0
+        }, completion: { (done) in
+            self.newPrime.removeFromSuperview()
+        })
     }
     
     //Returns true if position is valid (above tray), false otherwise
@@ -100,6 +177,8 @@ class DragGamePlayViewController: UIViewController {
             return true
         }
     }
+    
+    
 
     /*
     // MARK: - Navigation
@@ -111,4 +190,14 @@ class DragGamePlayViewController: UIViewController {
     }
     */
 
+}
+
+extension UIView {
+    func shake() {
+        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.duration = 0.4
+        animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
+        layer.add(animation, forKey: "shake")
+    }
 }
