73ed975d616f82befc4846ca934e0bf13e6096cb
Pedro Sandoval <psandovalsegura@gmail.com>
2017-01-13 20:36:27 -0800

    Add new icon and main view

--- arithmetical/MainViewController.swift
+++ arithmetical/MainViewController.swift
@@ -9,11 +9,34 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var playButtonView: UIView!
+    @IBOutlet weak var myStatsButtonView: UIView!
+    @IBOutlet weak var settingsButtonView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        roundViewEdges(view: playButtonView)
+        roundViewEdges(view: myStatsButtonView)
+        roundViewEdges(view: settingsButtonView)
+        
+        //Main gradient
+        let gradient = CAGradientLayer()
+        gradient.frame = self.view.bounds
+        gradient.colors = [UIColor(red: 51.0/255.0, green: 153.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor, UIColor(red: 0.0/255.0, green: 186.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor]
+        gradient.startPoint = CGPoint(x: 0.5, y: 0.6)
+        self.view.layer.insertSublayer(gradient, at: 0)
+        
+    }
+    
+    func roundViewEdges(view: UIView) {
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 1
+        view.layer.shadowOffset = CGSize(width: 0, height: 1)
+        view.layer.shadowRadius = 0.02
+        view.layer.cornerRadius = 50
     }
 
     override func viewWillAppear(_ animated: Bool) {
